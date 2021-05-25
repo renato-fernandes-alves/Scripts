64a05ce8af6902f2ccc45149a9d1bf8f08f77aec
renato-fernandes-alves <61331075+renato-fernandes-alves@users.noreply.github.com>
2021-05-25 14:56:38 -0300

    Update modulo.sh

--- Atividade_Semana_14/modulo.sh
+++ Atividade_Semana_14/modulo.sh
@@ -1,9 +1,9 @@
 function removerT {
 	$(cat $1 | tr -d '[A-Z]' > remTR.txt)
-	echo "remTR.txt"
+	cat "remTR.txt"
 }
 
 function removerS {
 	$(cat $1 | sed 's/[0-9]//g' > remSED.txt)
-	echo "remSED.txt"
+	cat "remSED.txt"
 }
